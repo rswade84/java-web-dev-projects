71e434953981e3c9d2fc03870641712ca60d6c6e
Renaldo <renaldowade@gmail.com>
2024-09-05 15:51:43 -0500

    Java Intro & Data Types - Exercise

--- datatypes/datatype-examples/src/main/java/org/launchcode/TempConverter.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/TempConverter.java
@@ -3,6 +3,7 @@ package org.launchcode;
 import java.util.Scanner;
 
 public class TempConverter {
+
     public static void main(String[] args) {
         double fahrenheit;
         double celsius;
